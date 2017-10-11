9df612bb827421910350318e9588b299ee4fb452
mathieu <mathieu.hautebas@gmail.com>
2017-10-11 22:45:21 +0200

    xml stuff

--- include/client_tool.h
+++ include/client_tool.h
@@ -1,4 +1,11 @@
+#include <libxml/parser.h>
+
 #define LG_BUFFER	1024
+
 struct addrinfo *results;
+char  *ipaddr, *port, *bin2run;
+
 int init_client (int server, char *host, char *port, struct addrinfo **result);
 int exec_bin(int sock2server, const char* bin2exec);
+int is_leaf(xmlNode * node);
+void print_xml(xmlNode * node, int indent_len);
